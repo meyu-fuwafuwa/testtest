5871c94242facccd8486abde8867da7c58108380
meyu-fuwafuwa <105214319+meyu-fuwafuwa@users.noreply.github.com>
2023-08-27 13:25:15 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,51 @@
 // JavaScript Document
 
 //humberger
-$(".openbtn").click(function () {
-    $(this).toggleClass('active');
-	$('.nav').toggleClass('show');
+$(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+    $(".circle-bg").toggleClass('circleactive');//丸背景にcircleactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去
+    $(".circle-bg").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
+});
+
+//slick
+var $slide = $(".slide")
+  .slick({
+    fade: true,    // fedeオン
+    speed: 1500,   // 画像切り替えにかかる時間（ミリ秒）
+    autoplaySpeed: 3000,   // 自動スライド切り替え速度
+    arrows: false,         // 矢印表示・非表示
+    autoplay: true,        // 自動再生
+    slidesToShow: 1,       // スライド表示数
+    slidesToScroll: 1,     // スライドする数
+    infinite: true         // 無限リピート オン・オフ
+  });
+
+
+
+//slick(recruit)
+$(function () {
+  $('#js-slider-8').slick({
+    arrows: false, // 前・次のボタンを表示しない
+    dots: false, // ドットナビゲーションを表示しない
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 5000, // スライドさせるスピード（ミリ秒）
+    cssEase: 'linear',
+    slidesToShow: 1, // 表示させるスライド数
+    variableWidth: true, // スライド幅の自動計算を無効化
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        pauseOnDotsHover: false,
+        swipe: false,
+        swipeToSlide: false,
+        touchMove: false,
+  });
 });
 
 //pagetop

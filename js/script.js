32a4da574cd525fd4b6c2cd125ca72bcf86ac7d4
meyu-fuwafuwa <105214319+meyu-fuwafuwa@users.noreply.github.com>
2023-08-28 22:43:36 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,8 +13,9 @@ $("#g-nav a").click(function () {//ナビゲーションのリンクがクリッ
     $(".circle-bg").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
 });
 
-//slick
-var $slide = $(".slide")
+
+//slick(mainvisual)
+var $slide_mainvisual = $(".slide")
   .slick({
     fade: true,    // fedeオン
     speed: 1500,   // 画像切り替えにかかる時間（ミリ秒）
@@ -26,11 +27,37 @@ var $slide = $(".slide")
     infinite: true         // 無限リピート オン・オフ
   });
 
+//slick(pickup)
+var $slide_pickup = $(".slide-pickup")
+  .slick({
+    autoplay:true,
+    arrows: false,
+    centerMode: true,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    slidesToShow: 3,
+    speed: 1500,   // 画像切り替えにかかる時間（ミリ秒）
+    autoplaySpeed: 5000,   // 自動スライド切り替え速度
+	responsive: [
+      {
+        breakpoint: 1280, // 480〜767px以下のサイズに適用
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768, // 〜479px以下のサイズに適用
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
 
 
 //slick(recruit)
-$(function () {
-  $('#js-slider-8').slick({
+var $slide_recruit = $("#js-slider-8")
+  .slick({
     arrows: false, // 前・次のボタンを表示しない
     dots: false, // ドットナビゲーションを表示しない
     autoplay: true,
@@ -46,7 +73,6 @@ $(function () {
         swipeToSlide: false,
         touchMove: false,
   });
-});
 
 //pagetop
 $('#footer-pagetop').click(function () {

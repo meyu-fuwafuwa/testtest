0934dfbcc37f948a04fe62baded31ef4b147c054
meyu-fuwafuwa <105214319+meyu-fuwafuwa@users.noreply.github.com>
2023-08-31 21:55:38 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -74,14 +74,6 @@ var $slide_recruit = $("#js-slider-8")
         touchMove: false,
   });
 
-//pagetop
-$('#footer-pagetop').click(function () {
-    $('body,html').animate({
-        scrollTop: 0//ページトップまでスクロール
-    }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
-    return false;//リンク自体の無効化
-});
-
 
 //fadein
 $(function(){

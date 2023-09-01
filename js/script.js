71e6252418101ce04965a27664651d0390ae19b5
meyu-fuwafuwa <105214319+meyu-fuwafuwa@users.noreply.github.com>
2023-09-01 14:45:47 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,67 +13,14 @@ $("#g-nav a").click(function () {//ナビゲーションのリンクがクリッ
     $(".circle-bg").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
 });
 
-
-//slick(mainvisual)
-var $slide_mainvisual = $(".slide")
-  .slick({
-    fade: true,    // fedeオン
-    speed: 1500,   // 画像切り替えにかかる時間（ミリ秒）
-    autoplaySpeed: 3000,   // 自動スライド切り替え速度
-    arrows: false,         // 矢印表示・非表示
-    autoplay: true,        // 自動再生
-    slidesToShow: 1,       // スライド表示数
-    slidesToScroll: 1,     // スライドする数
-    infinite: true         // 無限リピート オン・オフ
+//pagetop
+$(function(){
+  var pagetop = $('#footer-pagetop');
+  pagetop.click(function () {
+     $('body, html').animate({ scrollTop: 0 }, 500);
+     return false;
   });
-
-//slick(pickup)
-var $slide_pickup = $(".slide-pickup")
-  .slick({
-    autoplay:true,
-    arrows: false,
-    centerMode: true,
-    pauseOnFocus: false,
-    pauseOnHover: false,
-    slidesToShow: 3,
-    speed: 500,   // 画像切り替えにかかる時間（ミリ秒）
-    autoplaySpeed: 5000,   // 自動スライド切り替え速度
-	responsive: [
-      {
-        breakpoint: 1280, // 480〜767px以下のサイズに適用
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 768, // 〜479px以下のサイズに適用
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  });
-
-
-//slick(recruit)
-var $slide_recruit = $("#js-slider-8")
-  .slick({
-    arrows: false, // 前・次のボタンを表示しない
-    dots: false, // ドットナビゲーションを表示しない
-    autoplay: true,
-    autoplaySpeed: 0,
-    speed: 5000, // スライドさせるスピード（ミリ秒）
-    cssEase: 'linear',
-    slidesToShow: 1, // 表示させるスライド数
-    variableWidth: true, // スライド幅の自動計算を無効化
-        pauseOnFocus: false,
-        pauseOnHover: false,
-        pauseOnDotsHover: false,
-        swipe: false,
-        swipeToSlide: false,
-        touchMove: false,
-  });
-
+});
 
 //fadein
 $(function(){

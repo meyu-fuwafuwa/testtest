da0547210f7202e7541f4329d5b6e7af288c3ee5
meyu-fuwafuwa <105214319+meyu-fuwafuwa@users.noreply.github.com>
2023-09-19 00:46:13 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,20 +1,5 @@
 // JavaScript Document
 
-
-
-//slick(mainvisual)
-var $slide_mainvisual = $(".slide")
-  .slick({
-    fade: true,    // fedeオン
-    speed: 1500,   // 画像切り替えにかかる時間（ミリ秒）
-    autoplaySpeed: 3000,   // 自動スライド切り替え速度
-    arrows: false,         // 矢印表示・非表示
-    autoplay: true,        // 自動再生
-    slidesToShow: 1,       // スライド表示数
-    slidesToScroll: 1,     // スライドする数
-    infinite: true         // 無限リピート オン・オフ
-  });
-
 //slick(news)
 var $slide_news = $(".slide-news")
   .slick({
